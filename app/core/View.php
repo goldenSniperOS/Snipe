b0352c7ede70294d60d084460925c1668c1e5aa8
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:56:35 -0500

    Format Code

--- app/core/View.php
+++ app/core/View.php
@@ -1,8 +1,11 @@
 <?php
-class View{
-	public static function render($view, $data = [], $layout = null) {
+
+class View {
+
+    public static function render($view, $data = [], $layout = null) {
         extract($data, EXTR_PREFIX_SAME, "wddx");
         $content = Config::path('app') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $view . '.php';
         require_once Config::path('app') . DIRECTORY_SEPARATOR . 'themes/layouts' . DIRECTORY_SEPARATOR . $layout . '.php';
     }
+
 }
