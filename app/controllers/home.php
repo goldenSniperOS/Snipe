3bf2b2b9cbdb4131fc4bab6853c1d421e1471c21
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:54:36 -0500

    Format code

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,30 +1,29 @@
 <?php
 
-	/*
-	|--------------------------------------------------------------------------
-	| Ejemplo de Controlador - Controlador Principal Home
-	|--------------------------------------------------------------------------
-	| 
-	| Este controlador es el controlador por defecto al igual que, su método index.
-	| El Controlador Home y el método index son obligatorios en la Aplicación, y este 
-	| último, es obligatorio en todos los controladores, debido a que se ejecutara por
-	| defecto si no se le coloca otra función que se declare
-	|
-	*/
+/*
+  |--------------------------------------------------------------------------
+  | Ejemplo de Controlador - Controlador Principal Home
+  |--------------------------------------------------------------------------
+  |
+  | Este controlador es el controlador por defecto al igual que, su método index.
+  | El Controlador Home y el método index son obligatorios en la Aplicación, y este
+  | último, es obligatorio en todos los controladores, debido a que se ejecutara por
+  | defecto si no se le coloca otra función que se declare
+  |
+ */
 
-class Home
-{
-	public function index()
-	{
-		$layout = "home";
-		$meta = array(
+class Home {
+
+    public function index() {
+        $layout = "home";
+        $meta = array(
             'title' => 'Home',
             'description' => 'El mejor framework creado para ayudar a nuestros usuarios a construir sus webs.',
             'keywords' => 'php, framework, mvc, cms',
             'author' => 'Snipe Framework Group',
             'robots' => 'All'
         );
-		View::render('home/index', ['meta' => $meta], $layout);
-	}
+        View::render('home/index', ['meta' => $meta], $layout);
+    }
 
 }
