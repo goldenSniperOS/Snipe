d5c02aa2768c249fb66142fc0704007c56f963e1
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:02:45 -0500

    Agregar los layouts

--- app/core/View.php
+++ app/core/View.php
@@ -1,7 +1,8 @@
 <?php
 class View{
-	public static function render($view,$data = []){
-		extract($data, EXTR_PREFIX_SAME, "wddx");
-		require_once Config::path('app').DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$view.'.php';
-	}
+	public static function render($view, $data = [], $layout = null) {
+        extract($data, EXTR_PREFIX_SAME, "wddx");
+        $content = Config::path('app') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $view . '.php';
+        require_once Config::path('app') . DIRECTORY_SEPARATOR . 'themes/layouts' . DIRECTORY_SEPARATOR . $layout . '.php';
+    }
 }
