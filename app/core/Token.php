e334e9b97f0e98ed24964df68188b248a1904a89
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:56:32 -0500

    Format Code

--- app/core/Token.php
+++ app/core/Token.php
@@ -1,33 +1,37 @@
 <?php
-	class Token{
-		public static function generate($token_name = null){
-			if($token_name){
-				return Session::put($token_name,md5(uniqid()));
-			}
-			return Session::put(Config::get('session/token_name'),md5(uniqid()));
-		}
 
-		public static function check($token,$token_name = null){
+class Token {
 
-			$tokenName = Config::get('session/token_name');
+    public static function generate($token_name = null) {
+        if ($token_name) {
+            return Session::put($token_name, md5(uniqid()));
+        }
+        return Session::put(Config::get('session/token_name'), md5(uniqid()));
+    }
 
-			if($token_name){
-				$tokenName = $token_name;
-			}
-			
-			if(Session::exists($tokenName) && $token === Session::get($tokenName)){
-				Session::delete($tokenName);
-				return true;
-			}
-			return false;
-		}
+    public static function check($token, $token_name = null) {
 
-		public static function destroyall(){
-			foreach ($_SESSION as $index => $value) {
-				if(strpos($index,'token') !== false){
-					unset($_SESSION[$index]);
-				}
-			}
-		}
-	}
+        $tokenName = Config::get('session/token_name');
+
+        if ($token_name) {
+            $tokenName = $token_name;
+        }
+
+        if (Session::exists($tokenName) && $token === Session::get($tokenName)) {
+            Session::delete($tokenName);
+            return true;
+        }
+        return false;
+    }
+
+    public static function destroyall() {
+        foreach ($_SESSION as $index => $value) {
+            if (strpos($index, 'token') !== false) {
+                unset($_SESSION[$index]);
+            }
+        }
+    }
+
+}
+
 ?>
