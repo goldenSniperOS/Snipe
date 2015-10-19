12185176e40d6ce9f790c7c417c735874670c00b
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:56:20 -0500

    Format Code

--- app/core/HTML.php
+++ app/core/HTML.php
@@ -1,18 +1,21 @@
 <?php
-class HTML{	
-	public function image($normalpath,$attrib = []){
-		$stringFinal = "<img src='".Config::path('public').'/'.$normalpath."'";
-		foreach ($attrib as $key => $value) {
-			$stringFinal .= " ".$key."='".$value."'";
-		}
-		return $stringFinal.">";
-	}
 
-	public function style($normalpath){
-		echo "<link rel='stylesheet' type='text/css' href='".Config::path('public').'/'.$normalpath."'>";
-	}
+class HTML {
 
-	public function script($normalpath){
-		return "<script type='text/javascript' src='".Config::path('public').'/'.$normalpath."'></script>";
-	}	
+    public function image($normalpath, $attrib = []) {
+        $stringFinal = "<img src='" . Config::path('public') . '/' . $normalpath . "'";
+        foreach ($attrib as $key => $value) {
+            $stringFinal .= " " . $key . "='" . $value . "'";
+        }
+        return $stringFinal . ">";
+    }
+
+    public function style($normalpath) {
+        echo "<link rel='stylesheet' type='text/css' href='" . Config::path('public') . '/' . $normalpath . "'>";
+    }
+
+    public function script($normalpath) {
+        return "<script type='text/javascript' src='" . Config::path('public') . '/' . $normalpath . "'></script>";
+    }
+
 }
