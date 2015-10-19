df0e04b8d78ff9767dbc43f83549f0654537cd86
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:54:53 -0500

    Format code

--- app/core/Config.php
+++ app/core/Config.php
@@ -1,31 +1,35 @@
-<?php 
-class Config
-{
-	public static function get($path=null){
-		if($path){
-			$config = require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'app.php';
-			$path = explode('/',$path);
-			foreach ($path as $bit) {
-				if(isset($config[$bit])){
-					$config = $config[$bit];
-				}
-			}
-			return $config;
-		}
-		return false;
-	}
-	public static function path($path=null){
-		if($path){
-			$config = require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'paths.php';
-			$path = explode('/',$path);
-			foreach ($path as $bit) {
-				if(isset($config[$bit])){
-					$config = $config[$bit];
-				}
-			}
-			return $config;
-		}
-		return false;
-	}
+<?php
+
+class Config {
+
+    public static function get($path = null) {
+        if ($path) {
+            $config = require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'app.php';
+            $path = explode('/', $path);
+            foreach ($path as $bit) {
+                if (isset($config[$bit])) {
+                    $config = $config[$bit];
+                }
+            }
+            return $config;
+        }
+        return false;
+    }
+
+    public static function path($path = null) {
+        if ($path) {
+            $config = require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'paths.php';
+            $path = explode('/', $path);
+            foreach ($path as $bit) {
+                if (isset($config[$bit])) {
+                    $config = $config[$bit];
+                }
+            }
+            return $config;
+        }
+        return false;
+    }
+
 }
+
 ?>
