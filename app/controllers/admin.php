bfdf713622f89660a771a956bd87c990fe0cc750
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:01:56 -0500

    Agregar los metas y los layouts

---
+++ app/controllers/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+class Admin
+{
+	public function index()
+	{
+		$layout = "admin";
+		$meta = array(
+            'title' => 'Administrador',
+            'description' => 'El mejor framework creado para ayudar a nuestros usuarios a construir sus webs.',
+            'keywords' => 'php, framework, mvc, cms',
+            'author' => 'Snipe Framework Group',
+            'robots' => 'All'
+        );
+		View::render('admin/index', ['meta' => $meta], $layout);
+	}
+
+}
