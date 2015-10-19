478b00fbef1f0a44031288f30440aabc554cc321
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:57:43 -0500

    Add locale

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,5 +1,14 @@
 <div class="logo">
-	<h2>Bienvenidos a Admin</h2>
-	<h1>SnipeFramework</h1>
+    <h2>Bienvenidos a Admin</h2>
+    <h1>SnipeFramework</h1>
+    <?php echo _("Hello"); ?>
 </div>
+
+<form name="myform" method="post">
+    <select name="locale" onchange="myform.submit();">
+        <option value="en" <?php if ($_COOKIE['locale'] == "en") echo "selected='selected' " ?> >English</option>
+        <option value="fr" <?php if ($_COOKIE['locale'] == "fr") echo "selected='selected' " ?> >Français</option>
+    </select>  
+</form>
+
 <a href="<?= URL::to('home') ?>" class="btn btn-lg btn-danger">Home</a>
