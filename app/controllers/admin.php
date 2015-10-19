ac0fba428f1c325a1f34c462b9e5e866820c3a53
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:54:25 -0500

    Format code

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -1,18 +1,17 @@
 <?php
 
-class Admin
-{
-	public function index()
-	{
-		$layout = "admin";
-		$meta = array(
+class Admin {
+
+    public function index() {
+        $layout = "admin";
+        $meta = array(
             'title' => 'Administrador',
             'description' => 'El mejor framework creado para ayudar a nuestros usuarios a construir sus webs.',
             'keywords' => 'php, framework, mvc, cms',
             'author' => 'Snipe Framework Group',
             'robots' => 'All'
         );
-		View::render('admin/index', ['meta' => $meta], $layout);
-	}
+        View::render('admin/index', ['meta' => $meta], $layout);
+    }
 
 }
