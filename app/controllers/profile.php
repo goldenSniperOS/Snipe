6619e6bc17e7c07d6222d78e7063754dbddfe4e4
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:54:31 -0500

    Format code

--- app/controllers/profile.php
+++ app/controllers/profile.php
@@ -1,18 +1,17 @@
 <?php
 
-class Profile
-{
-	public function index()
-	{
-		$layout = "profile";
-		$meta = array(
+class Profile {
+
+    public function index() {
+        $layout = "profile";
+        $meta = array(
             'title' => 'Profile',
             'description' => 'El mejor framework creado para ayudar a nuestros usuarios a construir sus webs.',
             'keywords' => 'php, framework, mvc, cms',
             'author' => 'Snipe Framework Group',
             'robots' => 'All'
         );
-		View::render('profile/index', ['meta' => $meta], $layout);
-	}
+        View::render('profile/index', ['meta' => $meta], $layout);
+    }
 
 }
