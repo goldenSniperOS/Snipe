e5ae0e5562e351959a725ed715e7f03f5949f23d
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:55:00 -0500

    Format code

--- app/core/Hash.php
+++ app/core/Hash.php
@@ -1,14 +1,17 @@
 <?php
-class Hash{
-	public static function make($string,$salt = ''){
-		return hash('sha256',$string.$salt);
-	}
 
-	public static function salt($lengt){
-		return mcrypt_create_iv($lengt);
-	}
+class Hash {
 
-	public static function unique() {
-		return self::make(uniqid());
-	}
+    public static function make($string, $salt = '') {
+        return hash('sha256', $string . $salt);
+    }
+
+    public static function salt($lengt) {
+        return mcrypt_create_iv($lengt);
+    }
+
+    public static function unique() {
+        return self::make(uniqid());
+    }
+
 }
