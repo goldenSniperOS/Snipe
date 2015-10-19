304ed42c01f8275aa3706206bd9c5e25010e2936
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:04:48 -0500

    Agregado para estructura de Layouts

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,47 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Snipe Framework</title>
-	<style type="text/css">
-		@import url(http://fonts.googleapis.com/css?family=Indie+Flower);
-		@import url(http://fonts.googleapis.com/css?family=Lobster);
-		body{
-			background-color: steelblue;
-		}
+<div class="logo">
+	<h2>Bienvenidos a Home</h2>
+	<h1>SnipeFramework</h1>
+	
+</div>
+<a href="<?= URL::to('profile') ?>" class="btn btn-lg btn-danger">Profile</a>
+<a href="<?= URL::to('admin') ?>" class="btn btn-lg btn-danger">Admin</a>
 
-		.logo{
-			position: absolute;
-			top:100px;
-			left:0;
-			bottom:0;
-			right:0;
-			margin: auto;
-			
-			width: 100%;
-			
-			/*Texto*/
-			text-align: center;
-			color: #fff;
-			font-size: 2em;
-		}
-
-		.logo h2{
-			font-family: 'Indie Flower', cursive;
-		}
-
-		.logo h1{
-			font-family: 'Lobster', cursive;
-		}
-	</style>
-</head>
-<body>
-	<div class="logo">
-		<h2>Bienvenidos a</h2>
-		<h1>SnipeFramework</h1>
-	</div>
-</body>
-</html>
 
 
 
