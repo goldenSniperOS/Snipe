7f6793527c5e53fcfaf7b7d875e80b1b0121167c
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:56:26 -0500

    Format Code

--- app/core/Input.php
+++ app/core/Input.php
@@ -1,29 +1,32 @@
-<?php 
-class Input{
+<?php
 
-	public static function exists($type = 'post'){
-		switch ($type) {
-			case 'post':
-				return (!empty($_POST)) ? true : false;
-				break;
-			case 'get':
-				return (!empty($_GET)) ? true : false;
-				break;			
-			default:
-				return false;
-				break;
-		}
-	}
-	public static function get($item){
-		if(isset($_POST[$item])){
-			return $_POST[$item];
-		}else if(isset($_GET[$item])){
-			return $_GET[$item];
-		}
-		return '';
-	}
+class Input {
 
-	public static function all(){
-		return $_POST;
-	}
+    public static function exists($type = 'post') {
+        switch ($type) {
+            case 'post':
+                return (!empty($_POST)) ? true : false;
+                break;
+            case 'get':
+                return (!empty($_GET)) ? true : false;
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    public static function get($item) {
+        if (isset($_POST[$item])) {
+            return $_POST[$item];
+        } else if (isset($_GET[$item])) {
+            return $_GET[$item];
+        }
+        return '';
+    }
+
+    public static function all() {
+        return $_POST;
+    }
+
 }
