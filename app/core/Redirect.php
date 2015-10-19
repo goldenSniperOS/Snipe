4788cd09c06862c6bc9ccd0c17d93b63b26048fd
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 15:56:29 -0500

    Format Code

--- app/core/Redirect.php
+++ app/core/Redirect.php
@@ -1,18 +1,21 @@
 <?php
-class Redirect{
-	public static function to($location = null){
-		if($location){
-			if(is_numeric($location)){
-				switch($location){
-					case 404:
-						header('HTTP/1.0 404 Not Found');
-						include Config::path('app').'/includes/errors/404.php';
-						exit();
-					break;
-				}
-			}
-			header('Location: '.URL::to($location));
-			exit();
-		}
-	}
+
+class Redirect {
+
+    public static function to($location = null) {
+        if ($location) {
+            if (is_numeric($location)) {
+                switch ($location) {
+                    case 404:
+                        header('HTTP/1.0 404 Not Found');
+                        include Config::path('app') . '/includes/errors/404.php';
+                        exit();
+                        break;
+                }
+            }
+            header('Location: ' . URL::to($location));
+            exit();
+        }
+    }
+
 }
