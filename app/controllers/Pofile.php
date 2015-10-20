6c815cd9def58a2f8f8d0375bec3e6141c8b8fb9
memocorredor <guillermo.corredor@gmail.com>
2015-10-19 21:04:28 -0500

    Change parameters

---
+++ app/controllers/Pofile.php
@@ -0,0 +1,17 @@
+<?php
+
+class Profile {
+
+    public function index() {
+        $layout = "profile";
+        $meta = array(
+            'title' => 'Profile',
+            'description' => 'El mejor framework creado para ayudar a nuestros usuarios a construir sus webs.',
+            'keywords' => 'php, framework, mvc, cms',
+            'author' => 'Snipe Framework Groups',
+            'robots' => 'All'
+        );
+        View::render('profile/index', ['meta' => $meta], $layout);
+    }
+
+}
