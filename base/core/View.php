0164c053258215e3a416e6ed38eb99cc6ebee249
Adrián Ruiz <aerf2007@gmail.com>
2016-06-19 20:31:43 -0500

    Some fixes - Approach Snipe 3 Stable

--- base/core/View.php
+++ base/core/View.php
@@ -3,13 +3,13 @@ class View {
     public static function render($view, $data = [], $layout = null) {
         extract($data, EXTR_PREFIX_SAME, "wddx");
         //Scripts Generales para tener Datos del Framework en Javascript
-        $script =  new ScriptEngine();
         $content = Path::to('app') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $view . '.php';
         if($layout){
         	require_once Path::to('app') . DIRECTORY_SEPARATOR . 'layouts' . DIRECTORY_SEPARATOR . $layout . '.php';
         }else{
         	require_once $content;
         }
+        $script =  new ScriptEngine();
     }
 
     public static function add($route,$data = []){
